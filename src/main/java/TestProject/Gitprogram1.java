0a20b17d091a9df486af5727f10fe27c3a239b2b
Raghavendakudal <32901406+Raghavendakudal@users.noreply.github.com>
2019-11-20 12:36:26 +0530

    Update Gitprogram1.java

--- src/main/java/TestProject/Gitprogram1.java
+++ src/main/java/TestProject/Gitprogram1.java
@@ -4,6 +4,8 @@ public class Gitprogram1 {
 
 	public static void main(String[] args) {
 		// Author : Raghavendra.k
+		//Date: 20-11-2019
+		//Title: printing hello world	
 		System.out.println("Hello world");
 		int x=10;
 		Long l=(long) x;
