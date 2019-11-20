5742b06e1e24bc1baf9008411f8491fbc065d70e
D1RQ <D1RQ@IN01LAP80205.area1.eurofins.local>
2019-11-20 11:14:18 +0530

    Add new sys Statement

--- src/main/java/TestProject/Gitprogram1.java
+++ src/main/java/TestProject/Gitprogram1.java
@@ -5,6 +5,9 @@ public class Gitprogram1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		int x=10;
+		Long l=(long) x;
+		System.out.println("value of long is "+l);
 	}
 
 }
